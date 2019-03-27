17ebcf0bd68b0c71220bb020c9fdd56b61f1e534
Previn Wong <previnwong@Previns-MacBook-Air.local>
2019-03-27 16:52:42 -0700

    talkdesk cu

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/support')
+def support():
+    return render_template('support.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
