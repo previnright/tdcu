4de268befc6e8d8002dccacf0fcc122cbf34adc1
Previn Wong <previnwong@previns-air.gateway.sonic.net>
2019-04-19 23:23:40 -0700

    callback

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ def hello_world():
 
 @app.route('/support')
 def support():
-    return render_template('support.html')
-
-@app.route('/call')
-@cross_origin()
-def call():
 
 	url = 'https://tdcu.talkdeskid.com/oauth/token'
 	body = 'scope=callback%3Awrite&grant_type=client_credentials'
@@ -28,13 +23,27 @@ def call():
 	r = requests.post(url, data=body, headers=headers)
 	json_data = json.loads(r.text)
 	data = json_data['access_token']
+
+	return render_template('support.html', data=data)
+
+@app.route('/call')
+@cross_origin()
+def call():
+
+	# url = 'https://tdcu.talkdeskid.com/oauth/token'
+	# body = 'scope=callback%3Awrite&grant_type=client_credentials'
+	# headers = {'Content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Basic ZmI0NDNhM2JkMWQ0NDczMWJhM2FmYzRjZDE0ZmE3ODc6Z0s1YVAwck5fZWVGdHdjd0ZhTzRzRXUyNDhVZlptcUZCNUI1YkFJNTFacHdwclFCTVY0dkhOeThiUjRBQXpscUM4NjVUZXF2LW95c2ZFbHNEalByX2c='}
+
+	# r = requests.post(url, data=body, headers=headers)
+	# json_data = json.loads(r.text)
+	# data = json_data['access_token']
 	# data = json.dumps(token)
 	# url2 = 'https://api.talkdeskapp.com/calls/callback'
 	# body2 = '{"talkdesk_phone_number":"+18555888924","contact_phone_number":"+14086494479"}'
 	# headers2 = {'Content-type': 'application/json', 'Authorization': 'Bearer ' + token}; 
 	# r2 = requests.post(url2, data=body2, headers=headers2)
 
-	return render_template('call.html', data=data)
+	return render_template('call.html')
 
 @app.route('/email')
 def email():
